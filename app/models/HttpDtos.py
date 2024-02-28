af34d92084de845d93d628c45a433d86c52d5d9d
zzarbttoo <qodbwls70@naver.com>
2024-02-28 12:11:45 +0900

    [feature]api 개발

--- app/models/HttpDtos.py
+++ app/models/HttpDtos.py
@@ -3,9 +3,42 @@ from dataclasses_json import dataclass_json
 from dataclasses_json import LetterCase
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass()
-class Request1:
-    id_seq: int
-    contract_seq: int
-    model_name:str
+@dataclass
+class Home:
+    address: str #주소 
+    construction_company:str #시공사
+    executing_agency:str #시행사
+    type_sale: bool #분양? -> true: 분양, false : 임대
+    is_private:bool #민간? -> true: 민간, false : 공공 
+    scale: str #규모별
+    sale_price: int #분양가(백만원)
+    number_of_sale_units: int #총분양가구수
+    number_of_unsold_units: int #미분양가구수(당월)
+    subscription_date_for_sale: str #분양 청약일
+    contract_closing_date: str #계약 마감일 
+    scheduled_move_in_date: str #입주 예정일 
+    is_completion_status: bool #준공? -> true : 준공 false: 미준공
+    land_type: str #택지 종류
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Page:
+    total: int  # 전체 페이지 수
+    current: int # 현재 페이지 
+
+from typing import List
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class HomeResponseWithPage:
+    homeList: List[Home]
+    page : Page
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class WholeHomeResponseList:
+    homeList: List[Home]
+
+
 
--- app/routes/items.py
+++ app/routes/items.py
@@ -1,24 +1,47 @@
 # controllers/items.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 
 router = APIRouter()
 
+from datetime import datetime
+from app.models.HttpDtos import Home, Page, HomeResponseWithPage, WholeHomeResponseList
+
 @router.get("/items/", description="""items 전체 목록 """)
-async def read_items():
-    return {"message": "Read all items"}
+async def read_items(response:Response):
+
+    homes_data = [
+        ["도두일동 2619-1(도두 네오하임)", "네오종합건설㈜", "㈜네오투자개발", True ,True, "84.01", 340, 64, 24, datetime.strptime("2016.10.24", "%Y.%m.%d"),  datetime.strptime("2016.10.24", "%Y.%m.%d"), datetime.strptime("2016.11.07", "%Y.%m.%d"), True, "민간"], 
+    ]
+    
+    response.status_code = status.HTTP_200_OK 
+    return WholeHomeResponseList(homeList=[Home(*data) for data in homes_data])
 
 @router.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"message": f"Read item with ID {item_id}"}
 
-@router.post("/items/")
-async def create_item():
-    return {"message": "Create a new item"}
 
-from app.models.HttpDtos import Request1
+@router.get("/items/page")
+async def read_item_as_page(response:Response, page:Page):
+    # 가상의 데이터라고 가정합니다.
+    page = Page(total=10, current=1)
+    homes_data = [
+        ["도두일동 2619-1(도두 네오하임)", "네오종합건설㈜", "㈜네오투자개발", True ,True, "84.01", 340, 64, 24, datetime.strptime("2016.10.24", "%Y.%m.%d"),  datetime.strptime("2016.10.24", "%Y.%m.%d"), datetime.strptime("2016.11.07", "%Y.%m.%d"), True, "민간"], 
+    ]
+
+    response.status_code = status.HTTP_200_OK
+    response_data = HomeResponseWithPage(page=page, homeList=[Home(*data) for data in homes_data])
+    
+    return  response_data
+
+
+
+# @router.post("/items/")
+# async def create_item():
+#     return {"message": "Create a new item"}
+
 
 @router.post("/items/temp")
-async def create_item_with_body(req:Request1):
-
+async def create_item_with_body(req:Home):
     print(req)
 
