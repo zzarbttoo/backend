37c1a67c6cb80086c92db160493dc3968cc67c22
qodbwls70 <qodbwls70@naver.com>
2024-02-28 00:37:23 +0000

    [env]Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$PYTHON_PROFILES_ACTIVE" == "dev" ]; then
-    ENV_FILE="app/config/dev.env"
+    ENV_FILE="backend/app/config/dev.env"
 elif [ "$PYTHON_PROFILES_ACTIVE" == "ops" ]; then
-    ENV_FILE="app/config/ops.env"
+    ENV_FILE="backend/app/config/ops.env"
 else
-    ENV_FILE="app/config/local.env"
+    ENV_FILE="backend/app/config/local.env"
 fi
 
 exec python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8080 --env-file "$ENV_FILE"
